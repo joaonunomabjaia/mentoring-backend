07d49ccc8406809de398aae7100b430a56dbabbe
Jose Julai Ritsure <ritsure.julai.jose@gmail.com>
2023-04-14 11:41:56 +0200

    Location, indicator, mentorship, healthfacility and partner repositories

---
+++ src/main/java/mz/org/fgh/mentoring/repository/tutor/TutorLocationRepository.java
@@ -0,0 +1,19 @@
+package mz.org.fgh.mentoring.repository.tutor;
+
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.repository.CrudRepository;
+import mz.org.fgh.mentoring.entity.location.TutorLocation;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TutorLocationRepository extends CrudRepository<TutorLocation, Long> {
+
+    @Override
+    List<TutorLocation> findAll();
+
+    @Override
+    Optional<TutorLocation> findById(@NotNull Long id);
+}
