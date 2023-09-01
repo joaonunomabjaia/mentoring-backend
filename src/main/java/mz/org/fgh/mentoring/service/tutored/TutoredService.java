96c54354041ef076207128c72421e6d768c865e1
Jose Julai Ritsure <ritsure.julai.jose@gmail.com>
2023-09-01 10:26:08 +0200

    Metadados tutorados

--- src/main/java/mz/org/fgh/mentoring/service/tutored/TutoredService.java
+++ src/main/java/mz/org/fgh/mentoring/service/tutored/TutoredService.java
@@ -1,9 +1,11 @@
 package mz.org.fgh.mentoring.service.tutored;
 
 import jakarta.inject.Singleton;
+import mz.org.fgh.mentoring.dto.tutored.TutoredDTO;
 import mz.org.fgh.mentoring.entity.tutored.Tutored;
 import mz.org.fgh.mentoring.repository.tutored.TutoredRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Singleton
@@ -14,7 +16,12 @@ public class TutoredService {
         this.tutoredRepository = tutoredRepository;
     }
 
-    public List<Tutored> findAll(){
-        return this.tutoredRepository.findAll();
+    public List<TutoredDTO> findAll(){
+        List<Tutored> tutoreds = this.tutoredRepository.findAll();
+        List<TutoredDTO> tutoredDTOS = new ArrayList<>(tutoreds.size());
+        for (Tutored tutored :tutoreds) {
+            tutoredDTOS.add(new TutoredDTO(tutored));
+        }
+        return tutoredDTOS;
     }
 }
